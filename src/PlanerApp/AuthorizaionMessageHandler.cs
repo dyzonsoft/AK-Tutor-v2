c89b7f17e33c748be55e5eac4353c12a29a030bb
dyžon <dyzon@email.cz>
2022-04-05 17:31:21 +0200

    vytvoreni projektu a inicializace MudBlazoru

---
+++ src/PlanerApp/AuthorizaionMessageHandler.cs
@@ -0,0 +1,23 @@
+﻿using Blazored.LocalStorage;
+
+public class AuthorizaionMessageHandler : DelegatingHandler
+{
+    private readonly ILocalStorageService _storage;
+
+    public AuthorizaionMessageHandler(ILocalStorageService storage)
+    {
+        _storage = storage;
+    }
+
+    protected async override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        if (await _storage.ContainKeyAsync("access-token"))
+        {
+            var token = await _storage.GetItemAsStringAsync("access-token");
+            request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+        }
+        Console.WriteLine("Handler called");
+        return await base.SendAsync(request, cancellationToken);
+    }
+
+}
---
+++ src/PlanerApp/Program.cs
@@ -0,0 +1,27 @@
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+using PlanerApp;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddHttpClient("PlanerApp.Api", client =>
+{
+    client.BaseAddress = new Uri("https://planerapp-api.azurewebsites.net");
+}).AddHttpMessageHandler<AuthorizaionMessageHandler>();
+builder.Services.AddTransient<AuthorizaionMessageHandler>();
+
+builder.Services.AddScoped(sp => sp.GetService<IHttpClientFactory>().CreateClient("PlanerApp.Api"));
+
+
+
+
+builder.Services.AddMudServices();
+builder.Services.AddBlazoredLocalStorage();
+
+await builder.Build().RunAsync();
