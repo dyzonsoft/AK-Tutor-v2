4dd931a846a926ad344b55e2edc69fb229701d9e
dyžon <dyzon@email.cz>
2022-04-06 16:28:12 +0200

    Auth Layout a vytvoření Login form

---
+++ src/PlanerApp/Components/Authentication/LoginForm.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using PlanerApp.Shared.Models;
+
+namespace PlanerApp.Components
+{
+    public partial class LoginForm : ComponentBase
+    {
+        [Inject]
+        public HttpClient HttpClient { get; set; }
+
+        private LoginRequest _model = new LoginRequest();
+
+        private async Task LoginUserAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
